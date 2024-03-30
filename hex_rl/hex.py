82def5de066824d2a74f69bbf229a44cf36da6c3
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2024-03-30 15:42:41 +0700

    fix a bug: grouping

--- hex_rl/hex.py
+++ hex_rl/hex.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import numpy as np
 from rich.console import Console
 
@@ -27,8 +28,8 @@ class HexCore:
         self.turn = 1
         self.winner = None
 
-        self._first_groups = list()
-        self._second_groups = list()
+        self._first_groups: list[set[tuple[int, int]]] = list()
+        self._second_groups: list[set[tuple[int, int]]] = list()
 
 
     def init_board(self) -> np.ndarray:
@@ -43,10 +44,11 @@ class HexCore:
             raise InvalidActionError(tup_action, self.board[tup_action])
 
         self.board[tup_action] = self.turn
-        self.turn *= -1
 
         self._add_to_and_merge_groups(tup_action)
         self.winner = self.check_winner()
+
+        self.turn *= -1
     
 
     def is_valid_action(self, tup_action: tuple[int, int]) -> bool:
@@ -69,33 +71,44 @@ class HexCore:
 
     def _add_to_and_merge_groups(self, tup_action: tuple[int, int]) -> None:
         if self.board[tup_action] == 1:
+            print('group1')
             self._first_groups = self._merge_groups(tup_action, self._first_groups)
         else:
+            print('group2')
             self._second_groups = self._merge_groups(tup_action, self._second_groups)
         
 
-    def _merge_groups(self, tup_action: tuple[int, int], groups: list) -> list:
+    def _merge_groups(self, tup_action: tuple[int, int], groups: list[set[tuple[int, int]]]
+                      ) -> list[set[tuple[int, int]]]:
         neighbors = self._get_neighbors(tup_action)
-        neighbors = list(filter(
-            lambda tup_action: self.board[tup_action] == self.turn,
-            neighbors
-        ))
 
         new_group = {tup_action}
+        old_groups = list()
         for neighbor in neighbors:
             for group in groups:
                 if neighbor in group:
                     new_group |= group
-                    groups.remove(group)
+                    old_groups.append(group)
                     break
-        
+
+        for group in old_groups:
+            groups.remove(group)
         groups.append(new_group)
+
         return groups
     
 
-    def ascii_render(self) -> str:
-        console = Console(highlight=False)
+    def _print_groups(self):
+        print("First groups:")
+        for group in self._first_groups:
+            print(group)
+        print("Second groups:")
+        for group in self._second_groups:
+            print(group)
 
+
+    def get_rich_str(self) -> str:
+        bold_dot = '[bold]\u22C5[/bold]'
         res = '    ' + '  '.join(f'{i:2d}' for i in range(self.size)) + '\n\n'
         res += '    ' + '[bold red]' + '-' * (self.size * 4 + 1) + '[/]' + '\n'
         for i in range(self.size):
@@ -106,31 +119,43 @@ class HexCore:
                 elif self.board[i, j] == -1:
                     res += ' [bold blue]O[/]  ' if j != self.size - 1 else ' [bold blue]O[/] '
                 else:
-                    res += ' .  ' if j != self.size - 1 else ' . '
+                    res += f' {bold_dot}  ' if j != self.size - 1 else f' {bold_dot} '
             if i == self.size - 1:
                 res += '[bold blue]\\\[/]\n'
             else:
                 res += '[bold blue]\\\[/]\n' + '  ' * i + f'      [bold blue]\\\[/]' + ' ' * (self.size * 4 - 1) + '[bold blue]\\\[/]\n'
-        res += '  ' * (self.size) + '    ' +  '[bold red]' + '-' * (self.size * 4 + 1) + '[/]' + '\n'
-        
-        console.print(res)
+        res += '  ' * (self.size) + '    ' +  '[bold red]' + '-' * (self.size * 4 + 1) + '[/]'
+
+        return res
+
+
+    def rich_render(self) -> str:
+        console = Console(highlight=False)
+        console.print(self.get_rich_str())
     
 
     def check_winner(self):
         pass
+    
 
 if __name__ == "__main__":
     hex = HexCore(13)
-    hex.ascii_render()
+    hex._print_groups()
+    hex.rich_render()
+    hex._print_groups()
+    print('===================================================')
 
     hex.play((0, 0))
-    hex.ascii_render()
+    hex.rich_render()
+    hex._print_groups()
+    print('===================================================')
 
     hex.play((1, 3))
-    hex.ascii_render()
+    hex.rich_render()
+    hex._print_groups()
+    print('===================================================')
 
     hex.play((0, 1))
-    hex.ascii_render()
+    hex.rich_render()
     # hex.play((1, 3))
-    print(hex._first_groups)
-    print(hex._second_groups)
+    hex._print_groups()
