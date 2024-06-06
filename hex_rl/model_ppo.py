f7a222086cfd11f8dab6e52c5fa3d3038312275f
Hoàng Trần Nhật Minh <82358580+htnminh@users.noreply.github.com>
2024-06-06 17:23:09 +0700

    ppo

--- hex_rl/model_ppo.py
+++ hex_rl/model_ppo.py
@@ -2,6 +2,8 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 
+from stable_baselines3 import PPO
+
 from hex import Hex, InvalidActionError
 
 class HexEnv(gym.Env):
@@ -10,27 +12,50 @@ class HexEnv(gym.Env):
         self.hex = hex
 
         self.action_space = spaces.Discrete(hex.size * hex.size)
-        self.observation_space = spaces.Box(low=-1, high=1, shape=(hex.size, hex.size), dtype=np.int)
+        self.observation_space = spaces.Box(low=-1, high=1, shape=(hex.size, hex.size), dtype=int)
 
     
+    def reset(self, seed=None):
+        self.hex.reset()
+        return self.hex.board, {}
+
+
     def step(self, action):
-        curr_player = hex.player
-        row, col = divmod(action, self.board_size)
+        # observation, reward, terminated, truncated, info 
+        curr_player = self.hex.player
+        row, col = divmod(action, self.hex.size)
         try:
             self.hex.play((row, col))
             
-        except InvalidActionError:
-            return self.state, -10, False, {}  # Invalid move
+        except InvalidActionError:  # Invalid move
+            return self.hex.board, -10, False, False, {}  
 
 
         if self.hex.winner == curr_player:
-            return self.state, 10, True, {}
+            return self.hex.board, 10, True, False, {}
         
         if self.hex.winner == -curr_player:
-            return self.state, -10, True, {}
+            return self.hex.board, -10, True, False, {}
         
 
-        return self.state, 0, False, {}
+        return self.hex.board, 0, False, False, {}
+
 
     def render(self, mode='human'):
-        print(self.state)
+        self.hex.rich_print()
+
+
+
+
+
+# Create the environment
+env = HexEnv(hex=Hex(size=5))
+
+# Instantiate the PPO agent
+model = PPO("MlpPolicy", env, verbose=1)
+
+# Train the agent
+model.learn(total_timesteps=10000)
+
+# Save the model
+model.save("ppo_hex")
